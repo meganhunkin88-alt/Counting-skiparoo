3932c23206845e038ed3fc901347e8a4c7e02aa2
meganhunkin88-alt <meganhunkin88@icloud.com>
2025-10-18 09:31:23 +0800

    Add service worker for PWA caching

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "skiparoo-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+});
